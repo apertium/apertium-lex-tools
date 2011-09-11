053d1ad2f0bcf1654972ca4e5e6f84a16f163e3f
Francis M. Tyers <ftyers@prompsit.com>
2011-09-11 22:02:39 +0000

    updates
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-lex-tools@33881 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ corpus/run-evaluation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LANG1="en"
+LANG2="ca"
+BASENAME="apertium-"$LANG1'-'$LANG2
+PAIR1=$LANG1'-'$LANG2
+PAIR2=$LANG2'-'$LANG1
+PAIRDIR=../../apertium-en-ca/
+EVAL=../scripts/apertium-lex-evaluate.py
+BILMODE=$PAIR2"-biltrans"
+TESTDIR="test"
+TEMPDIR="/tmp"
+
+## Frequency
+
+for i in $TESTDIR/*.$LANG2; do 
+	SRC=$TEMPDIR"/"`basename $i`".src";
+	TST=$TEMPDIR"/"`basename $i`".tst";
+	REF=`echo $i | sed "s/\.$LANG2//g"`;
+	cat $i | cut -f2- | apertium-destxt | apertium -f none -d $PAIRDIR $BILMODE > $SRC;
+	cat $SRC | python ../apertium-lex-freq.py ../freq.txt > $TST;	
+	RES=`python $EVAL $SRC $REF $TST 2>/dev/null`;
+	echo -e $RES"\tfreq\t"`basename $i`;
+	cat $SRC | ../apertium-lex-defaults $PAIRDIR/ca-en.autoldx.bin | python ../apertium-lex-freq.py ../freq.txt > $TST;	
+	RES=`python $EVAL $SRC $REF $TST 2>/dev/null`;
+	echo -e $RES"\tling\t"`basename $i`;
+done
+
+exit;
+for i in $TESTDIR/*.$LANG2; do 
+	rm $TEMPDIR"/"$i".src";
+	rm $TEMPDIR"/"$i".tst";
+done
+
+
