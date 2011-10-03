fdb3e08d3f021408672653127123174120350aba
Francis M. Tyers <ftyers@prompsit.com>
2011-10-03 10:21:20 +0000

    updates
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/apertium-lex-tools@34265 72bbbca6-d526-0410-a7d9-f06f51895060

--- corpus/run-evaluation.sh
+++ corpus/run-evaluation.sh
@@ -13,24 +13,30 @@ TEMPDIR="/tmp"
 
 ## Frequency
 
-for i in $TESTDIR/*.$LANG2; do 
+#for i in $TESTDIR/*.$LANG2; do 
+for i in $TESTDIR/prova.n_f.$LANG2; do 
 	SRC=$TEMPDIR"/"`basename $i`".src";
 	TST=$TEMPDIR"/"`basename $i`".tst";
 	REF=`echo $i | sed "s/\.$LANG2//g"`;
-	cat $i | cut -f2- | apertium-destxt | apertium -f none -d $PAIRDIR $BILMODE > $SRC;
-	cat $SRC | python ../apertium-lex-freq.py ../freq.txt > $TST;	
-	RES=`python $EVAL $SRC $REF $TST 2>/dev/null`;
-	RES1=`python $EVAL $SRC $REF $TST 2>/dev/null | cut -f2`;
+	#cat $i | cut -f2- | apertium-destxt | apertium -f none -d $PAIRDIR $BILMODE > $SRC;
+	cat $i | apertium-destxt | sed 's/\([0-9]*\)\[\t/[\1\t/g' | apertium -f none -d $PAIRDIR $BILMODE > $SRC;
+	cat $SRC | python ../apertium-lex-freq.py ../freq.txt > $TST".0";	
+	RES=`python $EVAL $SRC $REF $TST".0" 2>/dev/null`;
+	RES1=`python $EVAL $SRC $REF $TST".0" 2>/dev/null | cut -f2`;
 	#echo -e $RES"\tfreq\t"`basename $i`;
-	cat $SRC | ../apertium-lex-defaults $PAIRDIR/ca-en.autoldx.bin | python ../apertium-lex-freq.py ../freq.txt > $TST;	
-	RES=`python $EVAL $SRC $REF $TST 2>/dev/null`;
-	RES2=`python $EVAL $SRC $REF $TST 2>/dev/null | cut -f2`;
+	cat $SRC | ../apertium-lex-defaults $PAIRDIR/ca-en.autoldx.bin | python ../apertium-lex-freq.py ../freq.txt > $TST".1";	
+	RES=`python $EVAL $SRC $REF $TST".1" 2>/dev/null`;
+	RES2=`python $EVAL $SRC $REF $TST".1" 2>/dev/null | cut -f2`;
 	#echo -e $RES"\tling\t"`basename $i`;
-	cat $SRC | ../apertium-lex-defaults $PAIRDIR/dev/lex.f2e.top.bin | python ../apertium-lex-freq.py ../freq.txt > $TST;	
-	RES=`python $EVAL $SRC $REF $TST 2>/dev/null`;
-	RES3=`python $EVAL $SRC $REF $TST 2>/dev/null | cut -f2`;
+	cat $SRC | ../apertium-lex-defaults $PAIRDIR/dev/lex.f2e.top.bin | python ../apertium-lex-freq.py ../freq.txt > $TST".2";	
+	RES=`python $EVAL $SRC $REF $TST".2" 2>/dev/null`;
+	RES3=`python $EVAL $SRC $REF $TST".2" 2>/dev/null | cut -f2`;
+	#echo -e $RES"\tling\t"`basename $i`;
+	cat $SRC | python ../apertium-lex-rules.py ../rules.txt | python ../apertium-lex-freq.py ../freq.txt > $TST".3";
+	RES=`python $EVAL $SRC $REF $TST".3" 2>/dev/null`;
+	RES4=`python $EVAL $SRC $REF $TST".3" 2>/dev/null | cut -f2`;
 
-	echo -e `basename $i`"\t"$RES1"\t"$RES2"\t"$RES3;
+	echo -e `basename $i`"\t"$RES1"\t"$RES2"\t"$RES3"\t"$RES4;
 done
 
 exit;
@@ -38,5 +44,3 @@ for i in $TESTDIR/*.$LANG2; do
 	rm $TEMPDIR"/"$i".src";
 	rm $TEMPDIR"/"$i".tst";
 done
-
-
