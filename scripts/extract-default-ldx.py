03b0f064fbddd7dee80c8e41d781dbbbf4299f6b
Francis M. Tyers <ftyers@prompsit.com>
2012-07-31 11:17:23 +0000

    add defalut ldx script
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@39824 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ scripts/extract-default-ldx.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# coding=utf-8
+# -*- encoding: utf-8 -*-
+
+import sys;
+
+d = open(sys.argv[1]);
+sentit = sys.argv[2];
+
+poss_ambig = 0;
+sl = '';
+default = '';
+for line in d.readlines(): #{
+	line = line.strip();
+	if line.count('bet>') > 0 or  line.count('ionary>') > 0 or line.count('sdef') > 0 or line.count('ection>') > 0: #{
+		print(line);
+	#}
+
+	if line.count('c="') > 0 and line.count('0"') > 0 and poss_ambig == 1: #{
+		poss_ambig = 0;	
+	#}
+	if line.count('c="') > 0 and line.count('0"') > 0 and poss_ambig == 0: #{
+		poss_ambig = 1;	
+		if sentit == 'lr': #{
+			sl = line.split('<l>')[1].split('</l>')[0];
+			default = line.split('<r>')[1].split('</r>')[0];
+		elif sentit == 'rl': #{
+			sl = line.split('<r>')[1].split('</r>')[0];
+			default = line.split('<l>')[1].split('</l>')[0];
+		#}
+	#}
+	
+	if line.count('slr="1') > 0 and poss_ambig == 1: #{
+		if sentit == 'lr': #{
+			nsl = line.split('<l>')[1].split('</l>')[0];
+		elif sentit == 'rl': #{
+			nsl = line.split('<r>')[1].split('</r>')[0];
+		#}
+
+		if nsl == sl: #{
+			#print(sl , default);
+			if sentit == 'lr': #{
+				print('<e><p><l>' + sl + '</l><r>' + default + '</r></p></e>');
+			elif sentit == 'rl': #{
+				print('<e><p><l>' + default + '</l><r>' + sl + '</r></p></e>');
+			#}
+		#}
+	#}
+#}
