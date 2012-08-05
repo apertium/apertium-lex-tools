eee409a1852cb1ef4dbe9290c1052a362ecf3b2a
Francis M. Tyers <ftyers@prompsit.com>
2012-08-05 10:43:34 +0000

    add biltrans extract script
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@39958 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ scripts/extract-biltrans-cand.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# coding=utf-8
+# -*- encoding: utf-8 -*-
+
+import sys, codecs, copy;
+
+inf = open(sys.argv[1]);
+count = 0;
+for line in inf.readlines(): #{
+	if count == 2: #{
+		row = line.strip().split('\t');
+		outl = row[1];
+		outl = outl.replace(' ', '$ ^');
+		outl = '^' + outl + '$';
+		outl = outl.replace('~', ' ');
+
+		print row[0] + '\t' + outl;
+	#}
+	if line.count('-----------------------------') > 0: #{
+		count = 0;
+	#}
+	count = count + 1;
+#}
