1230deb547cc9a6eae8eb4e6786fabc13b2cc05b
Francis M. Tyers <ftyers@prompsit.com>
2012-08-23 15:11:01 +0000

    check for rubbish lines
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@40775 72bbbca6-d526-0410-a7d9-f06f51895060

--- scripts/extract-alig-lrx.py
+++ scripts/extract-alig-lrx.py
@@ -15,10 +15,14 @@ for line in d.readlines(): #{
 		sl = row[1];
 		tl = row[2];
 
+		if line.count('>') < 2: #{
+			continue;
+		#}
+
 		sl_lem = sl.split('<')[0];
 		tl_lem = tl.split('<')[0];
-		sl_lem = sl_lem.replace('-', '\\-').replace('~', ' ');
-		tl_lem = tl_lem.replace('-', '\\-').replace('~', ' ');
+		sl_lem = sl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
+		tl_lem = tl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
 		sl_tag = sl.replace('><', '.').split('<')[1].strip('>');
 		tl_tag = tl.replace('><', '.').split('<')[1].strip('>');
 		rule = '<rule comment="' + fq + '">';
