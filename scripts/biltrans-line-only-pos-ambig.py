238f6f31d0250f53eac1decc59b7d2ae23bd0e51
Francis M. Tyers <ftyers@prompsit.com>
2012-09-25 21:08:38 +0000

    another script
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@41298 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ scripts/biltrans-line-only-pos-ambig.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# coding=utf-8
+# -*- encoding: utf-8 -*-
+
+import sys;
+
+pos = ["<n>", "<vblex>", "<adj>"];
+
+def process_line(l): #{
+	global pos;
+	w = '';
+	in_word = False;
+	escaped = False;
+	for c in l: #{
+		if c == '\\': #{
+			escaped = True;
+			continue;
+		#}
+		if c == '^' and escaped == False: #{
+			in_word = True;
+		#}
+		if c == '$' and escaped == False: #{
+			word_in_pos = False;
+			for p in pos: #{
+				if w.count(p) > 0: #{
+					word_in_pos = True;
+				#}
+			#}
+			
+			if w.count('/') > 1 and word_in_pos == True: #{
+				return True;
+			#}
+			w = '';
+			in_word = False;
+		#}
+		if in_word == True: #{
+			w = w + c;
+		#}
+		escaped = False;
+	#}
+	return False;	
+#}
+
+output = False;	
+
+infile = sys.stdin ; 
+
+if len(sys.argv) > 1: #{
+	infile = open(sys.argv[1]);
+#}
+
+for line in infile.readlines(): #{
+
+	output = process_line(line);
+		
+	if output == True: #{
+		print(line.strip());
+	#}
+#}
