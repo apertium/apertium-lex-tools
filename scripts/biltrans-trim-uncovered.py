c6b13a2160d28de1784de8cbbfc32c134bdbdfc1
Francis M. Tyers <ftyers@prompsit.com>
2012-09-27 13:04:38 +0000

    Ad script
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@41307 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ scripts/biltrans-trim-uncovered.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# coding=utf-8
+# -*- encoding: utf-8 -*-
+
+import sys;
+
+pos = ["<n>", "<vblex>", "<adj>"];
+
+output = False;	
+
+infile = sys.stdin ; 
+
+if len(sys.argv) > 1: #{
+	infile = open(sys.argv[1]);
+#}
+
+lineno = 0;
+for line in infile.readlines(): #{
+	lineno = lineno + 1;
+	num_lu = float(line.count('$'));
+	num_unk = float(line.count('*'));
+	cov = 100.0 - ((num_unk / num_lu) * 100.0);
+	
+	if cov >= 90.0: #{
+		print(line.strip());
+	#}
+
+	print('%d %f' % (lineno, cov), file=sys.stderr);
+#}
