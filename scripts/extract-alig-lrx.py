9937fce8f23a46402dce94f8c98d52e3a1fd67f1
Filip Petkovski <filpetkovski@gmail.com>
2013-07-03 08:05:26 +0000

    tag processing
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@45523 72bbbca6-d526-0410-a7d9-f06f51895060

--- scripts/extract-alig-lrx.py
+++ scripts/extract-alig-lrx.py
@@ -8,44 +8,47 @@ d = open(sys.argv[1]);
 
 print('<rules>');
 for line in d.readlines(): #{
-#	print (line)
-	sys.stdout.flush();
-	if line[-2] == '@': #{
-		row = line.split(' ');
+	try:
+		sys.stdout.flush();
+		if line[-2] == '@': #{
+			row = line.split(' ');
 	
-		fq = row[0];
-		sl = row[1];
-		tl = row[2];
+			fq = row[0];
+			sl = row[1];
+			tl = row[2];
 
-		if line.count('>') < 2: #{
-			continue;
-		#}
+			if line.count('>') < 2: #{
+				continue;
+			#}
 
-		sl_lem = sl.split('<')[0];
-		tl_lem = tl.split('<')[0];
-		sl_lem = sl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
-		tl_lem = tl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
-		sl_tag = sl.replace('><', '.').split('<')[1].strip('>');
-		tl_tag = tl.replace('><', '.').split('<')[1].strip('>');
+			sl_lem = sl.split('<')[0];
+			tl_lem = tl.split('<')[0];
+			sl_lem = sl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
+			tl_lem = tl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
 
-		cmb = '';
-		cma = '';
-		if sl_tag not in ['adj', 'vblex', 'n']: #{
-			cmb = '<!--';
-			cma = '-->';	
-		else: #{
-			cma = '';
+			sl_tag = sl.replace('><', '.').split('<')[1].strip('>');
+			tl_tag = tl.replace('><', '.').split('<')[1].strip('>');
+
 			cmb = '';
-		#}
+			cma = '';
+			if sl_tag not in ['adj', 'vblex', 'n']: #{
+				cmb = '<!--';
+				cma = '-->';	
+			else: #{
+				cma = '';
+				cmb = '';
+			#}
 
-		rule = cmb + '<rule comment="' + fq + '">';
-		#rule = rule + '<match lemma="' + sl_lem + '" tags="' + sl_tag + '"><select lemma="' + tl_lem + '" tags="' + tl_tag + '"/>';	
-		rule = rule + '<match lemma="' + sl_lem + '"><select lemma="' + tl_lem + '"/>';	
-		rule = rule + '</match>';
-		rule = rule + '</rule>' + cma;
+			rule = cmb + '<rule comment="' + fq + '">';
+			#rule = rule + '<match lemma="' + sl_lem + '" tags="' + sl_tag + '"><select lemma="' + tl_lem + '" tags="' + tl_tag + '"/>';	
+			rule = rule + '<match lemma="' + sl_lem + '"><select lemma="' + tl_lem + '"/>';	
+			rule = rule + '</match>';
+			rule = rule + '</rule>' + cma;
 		
-		print(rule);
-	#}
+			print(rule);
+		#}
+	except:
+		pass
 
 #}
 print('</rules>');
