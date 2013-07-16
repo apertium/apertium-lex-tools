5c28621698b2c2276cdb984eba47e87d75b08bae
Filip Petkovski <filpetkovski@gmail.com>
2013-07-16 04:06:23 +0000

    bug fixes
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@45797 72bbbca6-d526-0410-a7d9-f06f51895060

--- multitrans.cpp
+++ multitrans.cpp
@@ -1,43 +1,59 @@
 #include "Multitrans.h"
 
+bool trim = false;
+bool filter = false;
+
+string path;
+string mode;
 
 void printError(char *name) {
 	wcout << "Usage: " << name << " ";
-	wcout << "<path to a binary bilingual transducer> <mode> [--trimmed | -t]" << endl;
+	wcout << "<path to a binary bilingual transducer> <mode> [options]" << endl;
 	wcout << "Modes: " << endl;
 	wcout << "  --biltrans | -b" << endl;
 	wcout << "  --multitrans | -m" << endl;
 	wcout << "  --trim-tagger-output | -p" << endl;
+
+	wcout << "Options: " << endl;
+	wcout << "  --filter-lines | -f" << endl;
+	wcout << "  --trim-lines | -t" << endl;
+
 }
 
-int main(int argc, char** argv) {
-
-	if (argc != 3 && argc != 4) {
+void parseArguments(int argc, char **argv) {
+	if (argc < 3 || argc > 5) {
 		printError(argv[0]);
 		exit(1);
 	}
-	string path(argv[1]);
-	string mode(argv[2]);
+	path = argv[1];
+	mode = argv[2];
 	
 	if ( mode == "--biltrans") {
 		mode = "-b";
 	} else if (mode == "--multitrans") {
 		mode = "-m";
-	} else if (mode == "--trim-tagger-output") {
+	} else if (mode == "--tagger-output") {
 		mode = "-p";
 	}
 	if(mode != "-b" && mode != "-m" && mode != "-p") {
 		printError(argv[0]);
 		exit(1);
 	}
-	bool trimmed = false;
 
-	if (argc == 4) {
-		string option(argv[3]);
-		trimmed = (option == "--trimmed" || option == "-t");
-	} else if (mode == "-p") {
-		trimmed = true;
+	for(int i = 2; i < argc; i++) {
+		if(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "-trim-lines") == 0) {
+			trim = true;
+		} else if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "-filter-lines") == 0) {
+			filter = true;
+		}
 	}
-	Multitrans mt(path, mode, trimmed);
+}
+
+
+int main(int argc, char** argv) {
+
+	parseArguments(argc, argv);
+
+	Multitrans mt(path, mode, trim, filter);
 	mt.processTaggerOutput();
 }
