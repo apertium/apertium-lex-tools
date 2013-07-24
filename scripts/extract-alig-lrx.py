8d39cfd89250947bbc22a0d2cbe7d39cd4f733eb
Filip Petkovski <filpetkovski@gmail.com>
2013-07-24 23:40:31 +0000

    ngrams-to-rules: threshold crispiness, not frequency ngram-prunning: defaults dont need to be skipped, thresholding takes care of it extract-alig-lrx: fix tag matching
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@46005 72bbbca6-d526-0410-a7d9-f06f51895060

--- scripts/extract-alig-lrx.py
+++ scripts/extract-alig-lrx.py
@@ -8,47 +8,47 @@ d = open(sys.argv[1]);
 
 print('<rules>');
 for line in d.readlines(): #{
-	try:
-		sys.stdout.flush();
-		if line[-2] == '@': #{
-			row = line.split(' ');
-	
-			fq = row[0];
-			sl = row[1];
-			tl = row[2];
 
-			if line.count('>') < 2: #{
-				continue;
-			#}
+	sys.stdout.flush();
+	if line[-2] == '@': #{
+		row = line.split(' ');
 
-			sl_lem = sl.split('<')[0];
-			tl_lem = tl.split('<')[0];
-			sl_lem = sl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
-			tl_lem = tl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
+		fq = row[0];
+		sl = row[1];
+		tl = row[2];
 
-			sl_tag = sl.replace('><', '.').split('<')[1].strip('>');
-			tl_tag = tl.replace('><', '.').split('<')[1].strip('>');
-
-			cmb = '';
-			cma = '';
-			if sl_tag not in ['adj', 'vblex', 'n']: #{
-				cmb = '<!--';
-				cma = '-->';	
-			else: #{
-				cma = '';
-				cmb = '';
-			#}
-
-			rule = cmb + '<rule comment="' + fq + '">';
-			#rule = rule + '<match lemma="' + sl_lem + '" tags="' + sl_tag + '"><select lemma="' + tl_lem + '" tags="' + tl_tag + '"/>';	
-			rule = rule + '<match lemma="' + sl_lem + '"><select lemma="' + tl_lem + '"/>';	
-			rule = rule + '</match>';
-			rule = rule + '</rule>' + cma;
-		
-			print(rule);
+		if line.count('>') < 2: #{
+			continue;
 		#}
-	except:
-		pass
+
+		sl_lem = sl.split('<')[0];
+		tl_lem = tl.split('<')[0];
+		sl_lem = sl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
+		tl_lem = tl_lem.replace('-', '\\-').replace('~', ' ').replace('&', '&amp;');
+
+		sl_tag = sl.replace('><', '.').split('<')[1].strip('>');
+		tl_tag = tl.replace('><', '.').split('<')[1].strip('>');
+
+		cmb = '';
+		cma = '';
+
+		if sl_tag.split('.')[0] not in ['adj', 'vblex', 'n']: #{
+			cmb = '<!--';
+			cma = '-->';	
+		else: #{
+			cma = '';
+			cmb = '';
+		#}
+
+		rule = cmb + '<rule comment="' + fq + '">';
+		#rule = rule + '<match lemma="' + sl_lem + '" tags="' + sl_tag + '"><select lemma="' + tl_lem + '" tags="' + tl_tag + '"/>';	
+		rule = rule + '<match lemma="' + sl_lem + '"><select lemma="' + tl_lem + '"/>';	
+		rule = rule + '</match>';
+		rule = rule + '</rule>' + cma;
+	
+		print(rule);
+	#}
+	
 
 #}
 print('</rules>');
