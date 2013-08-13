894034750169331f72164b9de1d1aa1294e77e53
Filip Petkovski <filpetkovski@gmail.com>
2013-08-13 07:23:28 +0000

    pareto trimming
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@46423 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ scripts/ngram-pareto-trim.py
@@ -0,0 +1,82 @@
+#!/bin/python
+
+import sys
+
+ngrams = {}
+translations = {}
+
+def make_line(ngrams, sl, ngram, tl):
+	return '+ ' + str(ngrams[sl][ngram][tl]) + '\t' + sl + '\t' + ngram + '\t' + tl + '\t1'
+
+def dominates(xs, ys):
+	n = len(xs)
+	fst = True;
+	snd = True;
+	for i in range (0, n):
+		if xs[i] > ys[i]:
+			snd = False;
+		if ys[i] > xs[i]:
+			fst = False;
+	
+	fst = int(fst)
+	snd = int(snd)
+	
+	if fst > snd:
+		return 1;
+	elif snd > fst:
+		return -1;
+	else:
+		return 0
+
+def calculate_pareto_frontier(ngrams, translations):
+	pareto = set();
+	m = []
+	for tl in translations:
+		v = []
+		for ngram in ngrams:
+			if tl in ngrams[ngram]:
+				v.append(ngrams[ngram][tl])
+			else:
+				v.append(0)
+		m.append((tl, v))
+
+	print (m)
+	for x in m:
+		if all(map(lambda y: dominates(x[1], y[1]) >= 0, m)):
+			pareto.add(x[0])
+	print (pareto)
+	return pareto
+	
+
+for line in sys.stdin.readlines():
+	line = line.rstrip();
+	row = line.split('\t');
+
+	weight = float(row[0].split()[1]);
+	(sl, ngram, tl) = (row[1], row[2], row[3])
+	
+	if sl not in translations:
+		translations[sl] = set()
+	translations[sl].add(tl)
+
+	if sl not in ngrams:
+		ngrams[sl] = {}
+	if ngram not in ngrams[sl]:
+		ngrams[sl][ngram] = {}
+	if tl not in ngrams[sl][ngram]:
+		ngrams[sl][ngram][tl] = weight;
+
+
+for sl in ngrams:
+	pareto = calculate_pareto_frontier(ngrams[sl], translations[sl])
+	for ngram in ngrams[sl]:
+		for tl in ngrams[sl][ngram]:
+			if tl in pareto:
+				print (make_line(ngrams, sl, ngram, tl));
+	
+		
+	
+
+	
+
+	
