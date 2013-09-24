13f759d435cc7d1a2a35df9b81735224275e3c29
Filip Petkovski <filpetkovski@gmail.com>
2013-09-24 06:14:52 +0000

    working with large files
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@47579 72bbbca6-d526-0410-a7d9-f06f51895060

--- scripts/merge-all-lambdas.py
+++ scripts/merge-all-lambdas.py
@@ -3,6 +3,10 @@
 from subprocess import Popen, PIPE, STDOUT
 import sys;
 
+if len(sys.argv) != 2:
+	print ("Usage:", sys.argv[0], "<path to yasmet>")
+	sys.exit(-1); 
+
 tokens = {}
 for line in sys.stdin:
 	line = line.rstrip();
@@ -15,17 +19,23 @@ for line in sys.stdin:
 	tokens[token]['events'].append(row[2]);
 	
 for token in tokens:
-	out = tokens[token]['n'] + '\n';
-	for event in tokens[token]['events']:
-		out += event + '\n';
-	out = out.rstrip();
-	p_red = Popen(['/home/philip/Apertium/apertium-lex-tools/yasmet', '-red', '0'], stdin=PIPE, stdout=PIPE, stderr=PIPE);
-	p_class = Popen(['/home/philip/Apertium/apertium-lex-tools/yasmet'], stdin=PIPE, stdout=PIPE, stderr=PIPE);
+	print >>sys.stderr, token
+	with open('events', 'w') as tmp_file:
+		tmp_file.write(tokens[token]['n'] + '\n')
+		for event in tokens[token]['events']:
+			tmp_file.write(event.rstrip(' ').lstrip(' ') + '\n');
 
-	red_data = p_red.communicate(input=out)[0]
-	stdout_data = p_class.communicate(input=red_data)[0].replace(' ', "\t").rstrip().split('\n')
-	
+	with open('events', 'r') as infile:
+		outfile = open('lambdas', 'w');
+		p_red = Popen([sys.argv[1], '-red', '3'], stdin=infile, stdout=outfile, stderr=None);
+		p_red.wait();
+		outfile.close();
+		outfile = open('lambdas', 'r');
+		p_class = Popen([sys.argv[1]], stdin=outfile, stdout=PIPE, stderr=None);
+		stdout_data = p_class.communicate()[0].replace(' ', "\t").rstrip().split('\n')
+		print ('\n'.join(map(lambda x: token + '\t' + x, stdout_data)))
+		outfile.close();
 
-	print '\n'.join(map(lambda x: token + '\t' + x, stdout_data))
+
 	
 
