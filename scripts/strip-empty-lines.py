2d093336bc5f0af23216cb07f2d05ee1bafff4c3
Francis M. Tyers <ftyers@prompsit.com>
2014-08-08 16:29:41 +0000

    new script
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-lex-tools@56388 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ scripts/strip-empty-lines.py
@@ -0,0 +1,42 @@
+import sys ;
+
+#perl "$MOSESDECODER/clean-corpus-n.perl" data.$SL-$TL/$CORPUS.tagged $SL $TL "data.$SL-$TL/$CORPUS.tagged-clean" 1 40;
+
+
+prefix = sys.argv[1];
+sl = sys.argv[2];
+tl = sys.argv[3];
+outfix = sys.argv[4]
+
+sl_f = open(prefix + '.' + sl);
+tl_f = open(prefix + '.' + tl);
+
+sl_o = open(outfix + '.' + sl, 'w+');
+tl_o = open(outfix + '.' + tl, 'w+');
+
+reading = True;
+
+inlines = 0;
+outlines = 0;
+
+while reading: #{
+
+	slline = sl_f.readline();
+	tlline = tl_f.readline();
+
+	if not slline and not tlline: #{
+		break;
+	#}
+
+	if slline.strip() == '' or tlline.strip() == '': #{
+		inlines = inlines + 1;
+		continue;
+	else: #{
+		sl_o.write(slline);
+		tl_o.write(tlline);
+		outlines = outlines + 1;
+	#}
+	inlines = inlines + 1;
+#}
+
+print('in: %d, out: %d' % (inlines, outlines));
