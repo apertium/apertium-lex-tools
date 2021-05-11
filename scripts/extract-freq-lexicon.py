5ce0d0990c850e02e3ab0462e75106184753c54e
Vivek Vardhan Adepu <32814044+vivekvardhanadepu@users.noreply.github.com>
2021-05-12 01:41:02 +0530

    Scripts fixup (#70)
    * Check if alignments are empty
    * Make error message more verbose

--- scripts/extract-freq-lexicon.py
+++ scripts/extract-freq-lexicon.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 # -*- encoding: utf-8 -*-
 
-import sys, common;
+import sys, common
 
 # Read the corpus, make a note of all ambiguous words, their frequency and their possible translations
 
@@ -21,92 +21,96 @@ import sys, common;
 def wrap (x):
 	return '^' + x + '$'
 
-MAX_NGRAMS = 3;
+MAX_NGRAMS = 3
 
-cur_line = 0;
-lineno = 0;
-sl_tl = {};
-ngrams = {};
+cur_line = 0
+lineno = 0
+sl_tl = {}
+ngrams = {}
 
-cur_sl_row = [];
-cur_tl_row = [];
-cur_bt_row = [];
-cur_al_row = [];
+cur_sl_row = []
+cur_tl_row = []
+cur_bt_row = []
+cur_al_row = []
 
 if len(sys.argv) < 2: #{
-	print('extract-freq-lexicon.py <candidate sent>');
-	sys.exit(-1);
+	print('extract-freq-lexicon.py <candidate sent>')
+	sys.exit(-1)
 #}
 
 #for line in open(sys.argv[1]).readlines(): #{
 with open(sys.argv[1]) as infile:
 	for line in infile: #{
-		line = line.strip();
+		line = line.strip()
 		lineno += 1
 		if lineno % 5000 == 0: #{
-			sys.stderr.write('.');
+			sys.stderr.write('.')
 			if lineno % 100000 == 0: #{
-				sys.stderr.write(str(lineno)+'\n');
+				sys.stderr.write(str(lineno)+'\n')
 			#}
-			sys.stderr.flush();
+			sys.stderr.flush()
 		#}
-		if line[0] == '-': #{
-			# Read the corpus, make a note of all ambiguous words, their frequency and their possible translations
-			#
-			# sl_tl[sl_word][tl_word] = tl_freq
-			i = 0;
-			for slword in cur_sl_row: #{
-				if len(cur_bt_row[i]['tls']) > 1: #{
-					for al in cur_al_row: #{
-						if al == '':
-							continue
-						al_sl = int(al.split('-')[1]);
-						al_tl = int(al.split('-')[0]);
-						if al_sl != i: #{
-							continue;
+		try:
+			if line[0] == '-': #{
+				# Read the corpus, make a note of all ambiguous words, their frequency and their possible translations
+				#
+				# sl_tl[sl_word][tl_word] = tl_freq
+				i = 0
+				for slword in cur_sl_row: #{
+					if len(cur_bt_row[i]['tls']) > 1: #{
+						for al in cur_al_row: #{
+							if al == '':
+								continue
+							al_sl = int(al.split('-')[1])
+							al_tl = int(al.split('-')[0])
+							if al_sl != i: #{
+								continue
+							#}
+							if al_tl < len(cur_tl_row):
+								tlword = cur_tl_row[al_tl]
+							else:
+								tlword = cur_tl_row[-1]
+								print("alignment out",
+									"of",
+									"range", al_tl,
+									"not in",
+									"len(",
+									cur_tl_row,
+									")",
+									file=sys.stderr)
+							slword = slword
+							if slword not in sl_tl: #{
+								sl_tl[slword] = {}
+							#}
+							if tlword not in sl_tl[slword]: #{
+								sl_tl[slword][tlword] = 0
+							#}
+							sl_tl[slword][tlword] = sl_tl[slword][tlword] + 1
+							# print '+' , slword , tlword , sl_tl[slword][tlword], lineno
 						#}
-						if al_tl < len(cur_tl_row):
-							tlword = cur_tl_row[al_tl];
-						else:
-							tlword = cur_tl_row[-1]
-							print("alignment out",
-								"of",
-								"range", al_tl,
-								"not in",
-								"len(",
-								cur_tl_row,
-								")",
-								file=sys.stderr)
-						slword = slword;
-						if slword not in sl_tl: #{
-							sl_tl[slword] = {};
-						#}
-						if tlword not in sl_tl[slword]: #{
-							sl_tl[slword][tlword] = 0;
-						#}
-						sl_tl[slword][tlword] = sl_tl[slword][tlword] + 1;
-						# print '+' , slword , tlword , sl_tl[slword][tlword], lineno;
 					#}
+					i = i + 1
 				#}
-				i = i + 1;
+				cur_line = 0
+				continue
 			#}
-			cur_line = 0;
-			continue;
-		#}
 
-		line = line.split('\t')[1];
+			line = line.split('\t')[1]
 
-		if cur_line == 0: #{
-			cur_sl_row = common.tokenise_tagger_line(line);
-		elif cur_line == 1: #{
-			cur_bt_row = common.tokenise_biltrans_line(line);
-		elif cur_line == 2: #{
-			cur_tl_row = common.tokenise_tagger_line(line);
-		elif cur_line == 3:  #{
-			cur_al_row = line.split(' ');
-		#}
+			if cur_line == 0: #{
+				cur_sl_row = common.tokenise_tagger_line(line)
+			elif cur_line == 1: #{
+				cur_bt_row = common.tokenise_biltrans_line(line)
+			elif cur_line == 2: #{
+				cur_tl_row = common.tokenise_tagger_line(line)
+			elif cur_line == 3:  #{
+				cur_al_row = line.split(' ')
+			#}
 
-		cur_line = cur_line + 1;
+			cur_line = cur_line + 1
+		except Exception as e:
+			print("Error in line", lineno, ":", e, file=sys.stderr)
+			sys.exit(-1)
 	#}
 #}
 
@@ -114,23 +118,23 @@ for sl in sl_tl: #{
 
 	newtl = sorted(sl_tl[sl], key=lambda x: sl_tl[sl][x])
 	newtl.reverse()
-	first = True;
+	first = True
 	for tl in newtl: #{
 		if tl[0] == '*': #{
-			print('error: tl word unknown', tl,  file=sys.stderr);
-			continue;
+			print('Error: tl word unknown', tl,  file=sys.stderr)
+			continue
 		#}
-		first_tag_sl = sl.split('<')[1].split('>')[0].strip();
-		first_tag_tl = tl.split('<')[1].split('>')[0].strip();
+		first_tag_sl = sl.split('<')[1].split('>')[0].strip()
+		first_tag_tl = tl.split('<')[1].split('>')[0].strip()
 		if first_tag_sl != first_tag_tl: #{
-			print('error:', first_tag_sl, '!=', first_tag_tl, file=sys.stderr);
-			continue;
+			print('Error:', first_tag_sl, '!=', first_tag_tl, file=sys.stderr)
+			continue
 		#}
 		if first: #{
-			print(sl_tl[sl][tl] , wrap(sl) , wrap(tl) , '@');
+			print(sl_tl[sl][tl] , wrap(sl) , wrap(tl) , '@')
 			first = False
 		else: #{
-			print(sl_tl[sl][tl] , wrap(sl) , wrap(tl));
+			print(sl_tl[sl][tl] , wrap(sl) , wrap(tl))
 		#}
 	#}
 #}
