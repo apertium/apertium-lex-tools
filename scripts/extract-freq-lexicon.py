7a0de2869207e802e0151161038337528c25cdc5
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-06-17 15:25:56 +0530

    Scripts fixup: enclosing the code in functions

--- scripts/extract-freq-lexicon.py
+++ scripts/extract-freq-lexicon.py
@@ -2,7 +2,8 @@
 # coding=utf-8
 # -*- encoding: utf-8 -*-
 
-import sys, common
+import sys
+import common
 
 # Read the corpus, make a note of all ambiguous words, their frequency and their possible translations
 
@@ -13,128 +14,135 @@ import sys, common
 # ngrams[ngram][tl_word] = freq
 
 # 5 	Please<vblex><inf> rise<n> ,<cm> then<adv> ,<cm> for<pr> this<det><dem> minute<n> 's<gen> silence<n> .<sent>
-#5 	Please<vblex><inf>/Complacer<vblex><inf> rise<n><sg>/aumento<n><m><sg> ,<cm>/,<cm> then<adv>/entonces<adv> ,<cm>/,<cm> for<pr>/para<pr>/durante<pr> this<det><dem><sg>/este<det><dem><GD><sg> minute<n><sg>/minuto<n><m><sg> '<apos>/'<apos> *s/*s silence<n><sg>/silencio<n><m><sg> .<sent>/.<sent>
-#5 	Invitar<vblex> a<pr> todo<prn><tn> a<pr> que<cnjsub> prpers<prn><pro> poner<vblex> de<pr> pie<n> para<pr> guardar<vblex><inf> uno<det><ind> minuto<n> de<pr> silencio<n> .<sent>
-#5 	0-0 4-2 5-3 8-1 9-5 10-6 12-7 13-8 14-9 15-10
-#-------------------------------------------------------------------------------
+# 5 	Please<vblex><inf>/Complacer<vblex><inf> rise<n><sg>/aumento<n><m><sg> ,<cm>/,<cm> then<adv>/entonces<adv> ,<cm>/,<cm> for<pr>/para<pr>/durante<pr> this<det><dem><sg>/este<det><dem><GD><sg> minute<n><sg>/minuto<n><m><sg> '<apos>/'<apos> *s/*s silence<n><sg>/silencio<n><m><sg> .<sent>/.<sent>
+# 5 	Invitar<vblex> a<pr> todo<prn><tn> a<pr> que<cnjsub> prpers<prn><pro> poner<vblex> de<pr> pie<n> para<pr> guardar<vblex><inf> uno<det><ind> minuto<n> de<pr> silencio<n> .<sent>
+# 5 	0-0 4-2 5-3 8-1 9-5 10-6 12-7 13-8 14-9 15-10
+# -------------------------------------------------------------------------------
 
-def wrap (x):
-	return '^' + x + '$'
 
-MAX_NGRAMS = 3
+def wrap(x):
+    return '^' + x + '$'
 
-cur_line = 0
-lineno = 0
-sl_tl = {}
-ngrams = {}
 
-cur_sl_row = []
-cur_tl_row = []
-cur_bt_row = []
-cur_al_row = []
+def extract_freq_lexicon(canditates):
+    # MAX_NGRAMS = 3
 
-if len(sys.argv) < 2: #{
-	print('extract-freq-lexicon.py <candidate sent>')
-	sys.exit(-1)
-#}
+    cur_line = 0
+    lineno = 0
+    sl_tl = {}
+    # ngrams = {}
 
-#for line in open(sys.argv[1]).readlines(): #{
-with open(sys.argv[1]) as infile:
-	for line in infile: #{
-		line = line.strip()
-		lineno += 1
-		if lineno % 5000 == 0: #{
-			sys.stderr.write('.')
-			if lineno % 100000 == 0: #{
-				sys.stderr.write(str(lineno)+'\n')
-			#}
-			sys.stderr.flush()
-		#}
-		try:
-			if line[0] == '-': #{
-				# Read the corpus, make a note of all ambiguous words, their frequency and their possible translations
-				#
-				# sl_tl[sl_word][tl_word] = tl_freq
-				i = 0
-				for slword in cur_sl_row: #{
-					if len(cur_bt_row[i]['tls']) > 1: #{
-						for al in cur_al_row: #{
-							if al == '':
-								continue
-							al_sl = int(al.split('-')[1])
-							al_tl = int(al.split('-')[0])
-							if al_sl != i: #{
-								continue
-							#}
-							if al_tl < len(cur_tl_row):
-								tlword = cur_tl_row[al_tl]
-							else:
-								tlword = cur_tl_row[-1]
-								print("alignment out",
-									"of",
-									"range", al_tl,
-									"not in",
-									"len(",
-									cur_tl_row,
-									")",
-									file=sys.stderr)
-							slword = slword
-							if slword not in sl_tl: #{
-								sl_tl[slword] = {}
-							#}
-							if tlword not in sl_tl[slword]: #{
-								sl_tl[slword][tlword] = 0
-							#}
-							sl_tl[slword][tlword] = sl_tl[slword][tlword] + 1
-							# print '+' , slword , tlword , sl_tl[slword][tlword], lineno
-						#}
-					#}
-					i = i + 1
-				#}
-				cur_line = 0
-				continue
-			#}
+    cur_sl_row = []
+    cur_tl_row = []
+    cur_bt_row = []
+    cur_al_row = []
 
-			line = line.split('\t')[1]
+    # for line in open(sys.argv[1]).readlines(): #{
+    with open(canditates) as infile:
+        for line in infile:  # {
+            line = line.strip()
+            lineno += 1
+            if lineno % 5000 == 0:  # {
+                sys.stderr.write('.')
+                if lineno % 100000 == 0:  # {
+                    sys.stderr.write(str(lineno)+'\n')
+                # }
+                sys.stderr.flush()
+            # }
+            try:
+                if line[0] == '-':  # {
+                    # Read the corpus, make a note of all ambiguous words, their frequency and their possible translations
+                    #
+                    # sl_tl[sl_word][tl_word] = tl_freq
+                    i = 0
+                    for slword in cur_sl_row:  # {
+                        if len(cur_bt_row[i]['tls']) > 1:  # {
+                            for al in cur_al_row:  # {
+                                if al == '':
+                                    continue
+                                al_sl = int(al.split('-')[1])
+                                al_tl = int(al.split('-')[0])
+                                if al_sl != i:  # {
+                                    continue
+                                # }
+                                if al_tl < len(cur_tl_row):
+                                    tlword = cur_tl_row[al_tl]
+                                else:
+                                    tlword = cur_tl_row[-1]
+                                    print("alignment out",
+                                          "of",
+                                          "range", al_tl,
+                                          "not in",
+                                          "len(",
+                                          cur_tl_row,
+                                          ")",
+                                          file=sys.stderr)
+                                slword = slword
+                                if slword not in sl_tl:  # {
+                                    sl_tl[slword] = {}
+                                # }
+                                if tlword not in sl_tl[slword]:  # {
+                                    sl_tl[slword][tlword] = 0
+                                # }
+                                sl_tl[slword][tlword] = sl_tl[slword][tlword] + 1
+                                # print '+' , slword , tlword , sl_tl[slword][tlword], lineno
+                            # }
+                        # }
+                        i = i + 1
+                    # }
+                    cur_line = 0
+                    continue
+                # }
 
-			if cur_line == 0: #{
-				cur_sl_row = common.tokenise_tagger_line(line)
-			elif cur_line == 1: #{
-				cur_bt_row = common.tokenise_biltrans_line(line)
-			elif cur_line == 2: #{
-				cur_tl_row = common.tokenise_tagger_line(line)
-			elif cur_line == 3:  #{
-				cur_al_row = line.split(' ')
-			#}
+                line = line.split('\t')[1]
 
-			cur_line = cur_line + 1
-		except Exception as e:
-			print("Error in line", lineno, ":", e, file=sys.stderr)
-			sys.exit(-1)
-	#}
-#}
+                if cur_line == 0:  # {
+                    cur_sl_row = common.tokenise_tagger_line(line)
+                elif cur_line == 1:  # {
+                    cur_bt_row = common.tokenise_biltrans_line(line)
+                elif cur_line == 2:  # {
+                    cur_tl_row = common.tokenise_tagger_line(line)
+                elif cur_line == 3:  # {
+                    cur_al_row = line.split(' ')
+                # }
 
-for sl in sl_tl: #{
+                cur_line = cur_line + 1
+            except Exception as e:
+                print("Error in line", lineno, ":", e, file=sys.stderr)
+                sys.exit(-1)
+        # }
+    # }
 
-	newtl = sorted(sl_tl[sl], key=lambda x: sl_tl[sl][x])
-	newtl.reverse()
-	first = True
-	for tl in newtl: #{
-		if tl[0] == '*': #{
-			print('Error: tl word unknown', tl,  file=sys.stderr)
-			continue
-		#}
-		first_tag_sl = sl.split('<')[1].split('>')[0].strip()
-		first_tag_tl = tl.split('<')[1].split('>')[0].strip()
-		if first_tag_sl != first_tag_tl: #{
-			print('Error:', first_tag_sl, '!=', first_tag_tl, file=sys.stderr)
-			continue
-		#}
-		if first: #{
-			print(sl_tl[sl][tl] , wrap(sl) , wrap(tl) , '@')
-			first = False
-		else: #{
-			print(sl_tl[sl][tl] , wrap(sl) , wrap(tl))
-		#}
-	#}
-#}
+    for sl in sl_tl:  # {
+
+        newtl = sorted(sl_tl[sl], key=lambda x: sl_tl[sl][x])
+        newtl.reverse()
+        first = True
+        for tl in newtl:  # {
+            if tl[0] == '*':  # {
+                print('Error: tl word unknown', tl,  file=sys.stderr)
+                continue
+            # }
+            first_tag_sl = sl.split('<')[1].split('>')[0].strip()
+            first_tag_tl = tl.split('<')[1].split('>')[0].strip()
+            if first_tag_sl != first_tag_tl:  # {
+                print('Error:', first_tag_sl, '!=',
+                      first_tag_tl, file=sys.stderr)
+                continue
+            # }
+            if first:  # {
+                print(sl_tl[sl][tl], wrap(sl), wrap(tl), '@')
+                first = False
+            else:  # {
+                print(sl_tl[sl][tl], wrap(sl), wrap(tl))
+            # }
+        # }
+    # }
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:  # {
+        print('extract-freq-lexicon.py <candidate sent>')
+        exit(1)
+    # }
+    extract_freq_lexicon(sys.argv[1])
--- scripts/extract-sentences.py
+++ scripts/extract-sentences.py
@@ -2,99 +2,107 @@
 # coding=utf-8
 # -*- encoding: utf-8 -*-
 
-import sys, codecs
+import sys
+import codecs
 import common
 
-if len(sys.argv) < 3: #{
-	print('extact-sentences.py <phrasetable> <biltrans>')
-	sys.exit(-1)
-#}
 
-phrase_table = open(sys.argv[1])
-biltrans_out = open(sys.argv[2])
+def ambiguous(bt):  # {
+    # legislation<n><sg>/legislación<n><f><sg>/ordenamiento<n><m><sg>
 
-def ambiguous(bt): #{
-	# legislation<n><sg>/legislación<n><f><sg>/ordenamiento<n><m><sg>
+    ambig = False
+    for token in bt:  # {
+        tls = token['tls']
+        if len(tls) > 1:  # {
+            return True
+        # }
+    # }
 
-	ambig = False
-	for token in bt: #{
-		tls = token['tls']
-		if len(tls) > 1: #{
-			return True
-		#}
-	#}
-
-	return ambig
-#}
-
-reading = True
-lineno = 0
-total_valid = 0
-total_errors = 0
-
-not_ambiguous = []
-
-while reading: #{
-	try:
-		lineno = lineno + 1
-		pt_line = phrase_table.readline().strip()
-		bt_line = biltrans_out.readline().strip()
-
-		if not bt_line.strip() and not pt_line.strip(): #{
-			reading = False
-			break
-		elif not bt_line.strip() or not pt_line.strip(): #{
-			continue
-
-		#}
-		row = pt_line.split('|||')
-		bt = common.tokenise_biltrans_line(bt_line.strip())
-		sl = common.tokenise_tagger_line(row[1].strip())
-		tl = common.tokenise_tagger_line(row[0].strip())
-		
-		if not ambiguous(bt): #{
-			not_ambiguous.append(str(lineno))
-			if len(not_ambiguous) >= 10: #{
-				print ("not ambiguous:", ' '.join(not_ambiguous), file=sys.stderr)
-				not_ambiguous = []
-			#}
-			continue
-		#}
-		if len(sl) < 2 and len(tl) < 2: #{
-			continue
-		#}
-
-		
-		# Check that the number of words in the lexical transfer, and in the phrasetable matches up
-		if len(sl) != len(bt): #{
-			print ("Error in line", lineno, ": len(sl) != len(bt)", file=sys.stderr)
-			continue
-		#}
-
-		# cheking if the alignments are empty
-		if not row[2].strip():
-			print("In line", lineno, ", alignments are empty", file=sys.stderr)
-			continue
-
-		# Resumption<n> of<pr> the<def><def> session<n>
-		# Resumption<n><sg>/Reanudación<n><f><sg> of<pr>/de<pr> the<det><def><sp>/el<det><def><GD><ND> session<n><sg>/sesión<n><f><sg>
-		# Reanudación<n> de<pr> el<det><def> periodo<n> de<pr> sesión<n>
-		# 0-0 1-1 2-2 5-3
+    return ambig
+# }
 
 
-		print(lineno, '\t' + row[1])
-		print(lineno, '\t' + bt_line)
-		print(lineno, '\t' + row[0])
-		print(lineno, '\t' + row[2])
-		print('-------------------------------------------------------------------------------')
-		total_valid += 1
-	except Exception as e:
-		print ("Error in line", lineno, ": ", e, file=sys.stderr)
-		total_errors += 1
-		continue
+def extract_sentences(phrase_table, biltrans_out):
+    reading = True
+    lineno = 0
+    total_valid = 0
+    total_errors = 0
 
-#}
+    not_ambiguous = []
 
-print('total:', lineno, file=sys.stderr)
-print('valid:', total_valid, '(' + str((total_valid/lineno)*100) + '%)', file=sys.stderr)
-print('errors:',total_errors, '(' + str((total_errors/lineno)*100) + '%)', file=sys.stderr)
+    while reading:  # {
+        try:
+            lineno = lineno + 1
+            pt_line = phrase_table.readline().strip()
+            bt_line = biltrans_out.readline().strip()
+
+            if not bt_line.strip() and not pt_line.strip():  # {
+                reading = False
+                break
+            elif not bt_line.strip() or not pt_line.strip():  # {
+                continue
+
+            # }
+            row = pt_line.split('|||')
+            bt = common.tokenise_biltrans_line(bt_line.strip())
+            sl = common.tokenise_tagger_line(row[1].strip())
+            tl = common.tokenise_tagger_line(row[0].strip())
+
+            if not ambiguous(bt):  # {
+                not_ambiguous.append(str(lineno))
+                if len(not_ambiguous) >= 10:  # {
+                    print("not ambiguous:", ' '.join(
+                        not_ambiguous), file=sys.stderr)
+                    not_ambiguous = []
+                # }
+                continue
+            # }
+            if len(sl) < 2 and len(tl) < 2:  # {
+                continue
+            # }
+
+            # Check that the number of words in the lexical transfer, and in the phrasetable matches up
+            if len(sl) != len(bt):  # {
+                print("Error in line", lineno,
+                      ": len(sl) != len(bt)", file=sys.stderr)
+                continue
+            # }
+
+            # cheking if the alignments are empty
+            if not row[2].strip():
+                print("In line", lineno, ", alignments are empty", file=sys.stderr)
+                continue
+
+            # Resumption<n> of<pr> the<def><def> session<n>
+            # Resumption<n><sg>/Reanudación<n><f><sg> of<pr>/de<pr> the<det><def><sp>/el<det><def><GD><ND> session<n><sg>/sesión<n><f><sg>
+            # Reanudación<n> de<pr> el<det><def> periodo<n> de<pr> sesión<n>
+            # 0-0 1-1 2-2 5-3
+
+            print(lineno, '\t' + row[1])
+            print(lineno, '\t' + bt_line)
+            print(lineno, '\t' + row[0])
+            print(lineno, '\t' + row[2])
+            print(
+                '-------------------------------------------------------------------------------')
+            total_valid += 1
+        except Exception as e:
+            print("Error in line", lineno, ": ", e, file=sys.stderr)
+            total_errors += 1
+            continue
+
+    # }
+
+    print('total:', lineno, file=sys.stderr)
+    print('valid:', total_valid,
+          '(' + str((total_valid/lineno)*100) + '%)', file=sys.stderr)
+    print('errors:', total_errors,
+          '(' + str((total_errors/lineno)*100) + '%)', file=sys.stderr)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:  # {
+        print('extact-sentences.py <phrasetable> <biltrans>')
+        exit(1)
+    # }
+    with open(sys.argv[1]) as phrase_table, open(sys.argv[2]) as biltrans_out:
+        extract_sentences(phrase_table, biltrans_out)
