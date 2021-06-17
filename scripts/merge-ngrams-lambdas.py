d4cba7372a5fbc855a4c687e15e32428d9514062
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-06-17 15:25:56 +0530

    Scripts fixup: enclosing the code in functions

--- scripts/merge-ngrams-lambdas.py
+++ scripts/merge-ngrams-lambdas.py
@@ -2,43 +2,52 @@
 # coding=utf-8
 # -*- encoding: utf-8 -*-
 
-import sys, codecs, random;
+import sys
+import codecs
+import random
 
-# ngram file
-# lambda file
-# lexicon file
 
-ngf = sys.argv[1];
-ldf = sys.argv[2];
+def merge_ngrams_lambdas(ngf, ldf):
+    # ngram file
+    # lambda file
+    # lexicon file
 
-ngrams = {};
-lambdas = {};
+    ngrams = {}
+    # lambdas = {}
 
-for line in open(ngf).readlines(): #{
-	#59763 	poor<adj> in<pr> capital<adj>
-	if len(line) < 2: #{
-		continue;
-	#}
-	row = line.strip().split( '\t' );
-	if(len(row) < 2):
-		row.append('');
-	ngid = int(row[0].strip());
-	ngrams[ngid] = row[1];
-#}
+    for line in open(ngf).readlines():  # {
+        # 59763 	poor<adj> in<pr> capital<adj>
+        if len(line) < 2:  # {
+            continue
+        # }
+        row = line.strip().split('\t')
+        if(len(row) < 2):
+            row.append('')
+        ngid = int(row[0].strip())
+        ngrams[ngid] = row[1]
+    # }
 
-with open(ldf) as d:
-	for line in d: #{
-		#59176:0 1.00131
-		if line.count('@@') > 0: #{
-			continue;
-		#}
-		row = line.strip().split('\t');
+    with open(ldf) as d:
+        for line in d:  # {
+            # 59176:0 1.00131
+            if line.count('@@') > 0:  # {
+                continue
+            # }
+            row = line.strip().split('\t')
 
-		l = float(row[2]);
-		ngid = int(row[1].split(':')[0]);
-		ngram = ngrams[ngid];
+            l = float(row[2])
+            ngid = int(row[1].split(':')[0])
+            ngram = ngrams[ngid]
 
-		trad = row[1].split(':')[1];
-		token = row[0]
-		print(token, '\t', l, '\t', trad, '\t', ngram);
-	#}
+            trad = row[1].split(':')[1]
+            token = row[0]
+            print(token, '\t', l, '\t', trad, '\t', ngram)
+        # }
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:  # {
+        print('merge-ngrams-lambdas.py <ngrams> <lambdas>')
+        exit(1)
+    # }
+    merge_ngrams_lambdas(sys.argv[1], sys.argv[2])
