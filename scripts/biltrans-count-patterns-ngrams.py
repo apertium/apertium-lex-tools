fe1ea473f7ef443c3f5b146d7d89f59aef39a757
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-07-31 20:30:46 +0530

    adding scripts(reqd for non-parallel corpora training) for packaging

--- scripts/biltrans-count-patterns-ngrams.py
+++ scripts/biltrans-count-patterns-ngrams.py
@@ -23,8 +23,8 @@ import biltrans_count_common as BCC
 
 #        c) Disambiguated biltrans output
 
-#.[][56011 0].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/excuse<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$ 0.9917274061    |@|
-#.[][56011 1].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/occasion<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$       0.0082725939    ||
+# .[][56011 0].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/excuse<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$ 0.9917274061    |@|
+# .[][56011 1].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/occasion<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$       0.0082725939    ||
 
 #	 d) Crispiness threshold
 
@@ -34,8 +34,8 @@ only_max = True
 #only_max = False
 
 if len(sys.argv) == 5:
-	crisphold = float(sys.argv[4])
-	print('crisp:', crisphold, file=sys.stderr)
+    crisphold = float(sys.argv[4])
+    print('crisp:', crisphold, file=sys.stderr)
 
 # First read in the frequency defaults
 
@@ -44,21 +44,23 @@ sl_tl, sl_tl_defaults, _ = BCC.read_frequencies(sys.argv[1])
 print('Reading...', file=sys.stderr)
 sys.stderr.flush()
 
+
 class Counter(BCC.BiltransCounter):
-	tokenizer = 'biltrans'
-	line_ids = True
-	count_ngrams = True
-	max_ngrams = 3
+    tokenizer = 'biltrans'
+    line_ids = True
+    count_ngrams = True
+    max_ngrams = 3
+
 
 c = Counter()
-c.read_files(sys.argv[2], # File with ambiguous biltrans output
-			 sys.argv[3]) # File with disambiguated biltrans output
+c.read_files(sys.argv[2],  # File with ambiguous biltrans output
+             sys.argv[3])  # File with disambiguated biltrans output
 ngrams = c.ngrams
 
 print('Caching counts...', file=sys.stderr)
 for sl in ngrams:
-	for ngram in ngrams[sl]:
-		for tl in ngrams[sl][ngram]:
-			print('%.10f\t%s\t%s\t%s' % (ngrams[sl][ngram][tl], ngram, sl, tl))
+    for ngram in ngrams[sl]:
+        for tl in ngrams[sl][ngram]:
+            print('%.10f\t%s\t%s\t%s' % (ngrams[sl][ngram][tl], ngram, sl, tl))
 
 print('\n', file=sys.stderr)
--- scripts/biltrans-extract-frac-freq.py
+++ scripts/biltrans-extract-frac-freq.py
@@ -12,33 +12,37 @@ import common
 # 56011   ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/excuse<n><f><sg>/occasion<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$
 #
 #        b) Disambiguated biltrans output
-#.[][56011 0].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/excuse<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$ 0.9917274061    |@|
-#.[][56011 1].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/occasion<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$       0.0082725939    ||
+# .[][56011 0].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/excuse<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$ 0.9917274061    |@|
+# .[][56011 1].[] ^un<det><ind><sp>/un<det><ind><GD><ND>$ ^digarez<n><m><sg>/occasion<n><f><sg>$ ^da<pr>/à<pr>$ ^distreiñ<vblex><inf>/revenir<vblex><inf>$ ^war<pr>/sur<pr>$ ^e<det><pos><m><sp>/son<det><pos><GD><ND>$ ^doare<n><m><sg>/manière<n><f><sg>$ ^ober<vblex><inf>/faire<vblex><inf>$ ^.<sent>/.<sent>$^.<sent>/.<sent>$       0.0082725939    ||
 #
 #
 
 # The sl-tl possible combinations
 sl_tl = defaultdict(lambda: defaultdict(lambda: 0.0))
 
-class Counter(BCC.BiltransCounter):
-	tokenizer = 'biltrans'
-	line_ids = True
 
-	def process_lu(self, sl, tl, idx, cur_sl_row, frac_count=0):
-		global sl_tl
-		sl_tl[sl][tl] += frac_count
+class Counter(BCC.BiltransCounter):
+    tokenizer = 'biltrans'
+    line_ids = True
+
+    def process_lu(self, sl, tl, idx, cur_sl_row, frac_count=0):
+        global sl_tl
+        sl_tl[sl][tl] += frac_count
+
 
 c = Counter()
-c.read_files(sys.argv[1], # File with ambiguous biltrans output
-			 sys.argv[2]) # File with disambiguated biltrans output
+c.read_files(sys.argv[1],  # File with ambiguous biltrans output
+             sys.argv[2])  # File with disambiguated biltrans output
 
 for sl in sl_tl:
-	newtl = sorted(sl_tl[sl], key=lambda x: sl_tl[sl][x])
-	newtl.reverse()
-	first = True
-	for tl in newtl:
-		if first:
-			print('%.10f %s %s @' % (sl_tl[sl][tl] , common.wrap(sl) , common.wrap(tl)))
-			first = False
-		else:
-			print('%.10f %s %s' % (sl_tl[sl][tl] , common.wrap(sl) , common.wrap(tl)))
+    newtl = sorted(sl_tl[sl], key=lambda x: sl_tl[sl][x])
+    newtl.reverse()
+    first = True
+    for tl in newtl:
+        if first:
+            print('%.10f %s %s @' %
+                  (sl_tl[sl][tl], common.wrap(sl), common.wrap(tl)))
+            first = False
+        else:
+            print('%.10f %s %s' %
+                  (sl_tl[sl][tl], common.wrap(sl), common.wrap(tl)))
